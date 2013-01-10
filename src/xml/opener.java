bbe9d82e7138167fee393cb60cab7363d33b7f8a
Thomas Traxler <thomas.traxler@student.tgm.ac.at>
2013-01-10 18:52:45 +0100

    jdom

--- src/xml/opener.java
+++ src/xml/opener.java
@@ -1,13 +1,19 @@
 package xml;
 
+import java.io.File;
+
+
 public class opener {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if(args[0] != null && new File(args[0]).isFile()){
+			
+		}else
+			System.out.println("Todo: Replace with synoptic");
+		
 	}
 
 }

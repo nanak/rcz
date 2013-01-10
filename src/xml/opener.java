81d0ee2008875d25cbdf23b33179c73ae6ab1115
XDDominik <Dominik.Backhausen@gmx.at>
2013-01-10 22:40:24 +0100

    Panel 1 und 2 erweitert und 3 erstellt

--- src/xml/opener.java
+++ src/xml/opener.java
@@ -9,12 +9,14 @@ public class opener {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		MyPanel3 mp3 = new MyPanel3();
+		new MyFrame(mp3,100,100,500,300,true);
 		boolean a =true;
 		if(args.length >0 ){
 			if(new File(args[0]).isFile()){ 
 				a=false;
 				Xml xml = new Xml(args[0]);
-				new MyFrame(new MyPanel1(),100,100,300,500);
+				new MyFrame(new MyPanel1(),100,100,300,500,true);
 			}else if(args[0]=="g"){
 				//TODO: Gui zur Fileauswahl
 			}
@@ -22,7 +24,7 @@ public class opener {
 		if(false)//TODO: Replace with if(a)
 			System.out.println("No valid Filepath\nTodo: Replace with synoptic");
 		
-		Xml xml = new Xml ("Rechenzentrum.xml");
+		//Xml xml = new Xml ("Rechenzentrum.xml");
 		
 	}
 

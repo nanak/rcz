62bffc6d3bf53fda4866230f3845b82f268706d3
XDDominik <Dominik.Backhausen@gmx.at>
2013-01-10 20:45:58 +0100

    GUI erweiter

--- src/xml/MyPanel1.java
+++ src/xml/MyPanel1.java
@@ -4,22 +4,29 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  * Diese Klasse ist für den Aufbau und das Anzeigen der Grafikelemente ZUständig
  * @author Dominik Backhausen
  * @version 0.3
  */
-public class MyPanel1 extends JPanel implements ActionListener{
-	private DefaultListModel<String> dlm;
-	private JList jl;
+public class MyPanel1 extends JPanel implements ActionListener, ListSelectionListener{
+	private DefaultListModel<String> dlm1, dlm2;
+	private JList jl1, jl2;
 	private JButton neu, edit;
+	private ArrayList<String> rz = new ArrayList<String>();
+	private ArrayList<ArrayList> sp = new ArrayList<ArrayList>();
 	
 	/**
 	 * StandartKonstruktor
@@ -27,8 +34,11 @@ public class MyPanel1 extends JPanel implements ActionListener{
 	public MyPanel1(){
 		this.setLayout(new BorderLayout());
 		
-		dlm = new DefaultListModel<String>();
-		jl = new JList(dlm);
+		dlm1 = new DefaultListModel<String>();
+		jl1 = new JList(dlm1);
+		jl1.addListSelectionListener(this);
+		dlm2 = new DefaultListModel<String>();
+		jl2 = new JList(dlm2);
 		
 		neu = new JButton("Neues Element");
 		edit = new JButton("Bearbeiten");
@@ -39,19 +49,44 @@ public class MyPanel1 extends JPanel implements ActionListener{
 		p1.add(neu);
 		p1.add(edit);
 		
+		JPanel p2 = new JPanel(new GridLayout(1,2,10,10));
+		p2.add(new JScrollPane(jl1));
+		p2.add(new JScrollPane(jl2));
+		
 		//Nach Bedarf kann auch noch eine Eingabe feld zum Ändern der Datei gemacht werden
 		
 		
 		// nur Themporär da die dafür benötigten Componenten Fehlen
 		for(int i = 0;i < 50; i ++){
-			dlm.addElement("Element " + (i+1));
+			rz.add("Rechenzentrum " + (i+1));
+			sp.add(new ArrayList<String>());
+			for(int ii = 0 ; ii < Math.random()+10;ii++){
+				sp.get(i).add( (i+1) + ". Supercomputer " + ii);
+			}
 		}
 		
-		this.add(new JScrollPane(jl));
+		this.add(p2);
 		this.add(p1,BorderLayout.SOUTH);
-		jl.setSelectedIndex(0);
+		this.updateList1();
+		jl1.setSelectedIndex(0);
+		this.updateList2(jl1.getSelectedIndex());
+		jl2.setSelectedIndex(0);
+		for(int i = 0; i < sp.get(3).size();i++){
+			System.out.println("" + i + " " + sp.get(3).get(i));
+		}
 	}
-
+	public void updateList1(){
+		dlm1.clear();
+		for(int i = 0; i < rz.size(); i ++){
+			dlm1.add(i, rz.get(i));
+		}
+	}
+	public void updateList2(int index){
+		dlm2.clear();
+		for(int i = 0; i < sp.get(index).size(); i ++){
+			dlm2.add(i,""+ sp.get(index).get(i));
+		}
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
@@ -59,7 +94,14 @@ public class MyPanel1 extends JPanel implements ActionListener{
 			System.out.println("Es wird ein neues Element Erstellt");// nur Themporär da die dafür benötigten Componenten Fehlen
 			
 		}else if(e.getSource() == edit){
-			System.out.println("Es wird " + dlm.get(jl.getSelectedIndex()) + " bearbeitet!");// nur Themporär da die dafür benötigten Componenten Fehlen
+			System.out.println("Es wird " + dlm1.get(jl1.getSelectedIndex()) + " bearbeitet!");// nur Themporär da die dafür benötigten Componenten Fehlen
 		}
+	}
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		if(e.getSource() == jl1){
+			this.updateList2(jl1.getSelectedIndex());
+		}
+		
 	}
 }
